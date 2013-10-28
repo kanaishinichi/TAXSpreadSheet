0dbc1db759b1603c717144c3f3e1e4eae5fdcf97
Shinichi Kanai <shinichi.kanai@twelve-axis.com>
2013-10-28 14:22:53 +0900

    Initial Commit

---
+++ TAXSpreadSheetDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TAXSpreadSheetDemo
+//
+//  Created by 金井 慎一 on 2013/10/28.
+//  Copyright (c) 2013年 Twelve Axis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TAXSpreadSheetDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TAXSpreadSheetDemo
+//
+//  Created by 金井 慎一 on 2013/10/28.
+//  Copyright (c) 2013年 Twelve Axis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
